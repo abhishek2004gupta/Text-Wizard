1fae4abbf5781baf4ce761b99a13fa6b2f180848
abhishek2004gupta <abhishekg272004@gmail.com>
2025-07-20 23:21:22 +0530

    data validation part implemented

---
+++ src/textWizard/pipeline/stage_02_data_validation.py
@@ -0,0 +1,14 @@
+from textWizard.config.configuration import ConfigurationManager
+from textWizard.components.data_validation import DataValidation
+from textWizard.logging import logger
+
+
+class DataValidationTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        data_validation_config = config.get_data_validation_config()
+        data_validation = DataValidation(config=data_validation_config)
+        data_validation.validate_all_files_exist()
